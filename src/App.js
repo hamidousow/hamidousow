6dfb8ce2788d7d0aec4a3b28cec1ecaf0c9ab1ad
Hamidou.S <86967645+hamidousow@users.noreply.github.com>
2021-10-29 22:43:21 +0200

    basic settings

--- src/App.js
+++ src/App.js
@@ -1,6 +1,14 @@
+import { NotFound } from "http-errors";
+import { Route, Switch } from "react-router";
+import Home from "./home/Home";
+
 function App() {
   return (
     <>
+    <Switch>
+      <Route path="/" exact component={Home}/>
+      <Route component={NotFound}/>
+    </Switch>
     </>
   );
 }
