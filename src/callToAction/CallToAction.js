f3d15bc50de1df79c6e276642ddfcf98afffa8ad
Hamidou.S <86967645+hamidousow@users.noreply.github.com>
2021-11-01 14:51:53 +0100

    create the call to action component

---
+++ src/callToAction/CallToAction.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const CallToAction = () => {
+    return (
+        <>
+            <div className="callToAction"> start </div>
+        </>
+    );
+};
+
+export default CallToAction;
