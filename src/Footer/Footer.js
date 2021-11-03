a36c0d09b90b11cb05a368746ac1dc4352819692
Hamidou.S <86967645+hamidousow@users.noreply.github.com>
2021-11-03 14:38:33 +0100

    footer component created

---
+++ src/Footer/Footer.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const Footer = () => {
+    return (
+        <footer className="section">
+            <div className="content">
+                <div className="containerIcons">
+                    
+                </div>
+            </div>
+        </footer>
+    );
+};
+
+export default Footer;
