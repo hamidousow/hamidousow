d3e7168731367b870d6285c470af8f8f89fbc4ec
Hamidou.S <86967645+hamidousow@users.noreply.github.com>
2021-11-03 14:37:01 +0100

    card mobile version created and fixed

--- src/CardProject/CardProject.js
+++ src/CardProject/CardProject.js
@@ -2,18 +2,18 @@ import React from 'react';
 
 const CardProject = () => {
     return (
-        <div className="card">
+        <div className="card" data-component="card">
             {/* <span className="projectNumber">01</span> */}
             <div className="cardContent">
-                <p className="projectNumber">01</p>                
+                <p className="projectNumber outlines">01</p>                
                 <div className="cardRichText">
-                    <h2 className="cardTitle">Titre du projet</h2>
+                    <h2 className="cardTitle gradient-title">Titre du projet</h2>
                     <h3 className="cardSubtitle">Subtitle</h3>                     
                 </div>
-                <div className="cardContainerImage"></div>
-            </div>
-            
-            
+                <div className="cardContainerImage">
+                    <img className="cardImg" src="./img/nisaa-header.jpg" alt="femme souriante prennant soin d'elle"/>
+                </div>
+            </div>           
         </div>
     );
 };
