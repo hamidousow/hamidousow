d1998b5fff9525264e106cfb0a5c178233fab851
Hamidou.S <86967645+hamidousow@users.noreply.github.com>
2021-11-08 13:36:26 +0100

    header project content

--- src/header/Header.js
+++ src/header/Header.js
@@ -3,13 +3,11 @@ import CallToAction from '../callToAction/CallToAction';
 
 const Header = () => {
     return (
-        <header className="heroSection"> 
+        <header className="header"> 
             <div className="content">          
                     <div className="leftContent">
-                        <h1 className="bigTitle">
-                            <span className="gradient-title"> Hello. </span>
-                            <br/>I am Hamidou</h1> 
-                        <p className="smallTitle">developer & ui / ux designer</p>          
+                        <h1 className="bigTitle"> Titre du projet </h1> 
+                        <p className="smallTitle">type de projet</p>          
                     </div>
                     <div className="rightContent">
                         <img src="" className="ImgSection"/>
--- src/pages/Nisaa.js
+++ src/pages/Nisaa.js
@@ -1,12 +1,13 @@
 import React from 'react';
-
 import Nav from '../nav/Nav';
+import Header from '../header/Header';
 
 
 const Nisaa = () => {
     return (
         <>
-        <Nav/>        
+        <Nav/>
+        <Header/>
         </>
     );
 };
