ee120604437fcd95828d3f4bf71f41ccadb44600
Hamidou.S <86967645+hamidousow@users.noreply.github.com>
2021-11-10 16:48:46 +0100

    button fixed

--- src/components/button/Button.js
+++ src/components/button/Button.js
@@ -1,13 +1,9 @@
-import React, {Component} from 'react';
+import React from 'react';
 
-class Button extends Component {
-    render() {
-        const { buttonContent } = this.props;
-        return (
-            <button className={buttonContent.className}>{buttonContent.text}</button>
-        )
-    }
-}
+const Button = (props) => {
+    return (
+        <button className="button">{props.text}</button>
+    );
+};
 
 export default Button;
-    
