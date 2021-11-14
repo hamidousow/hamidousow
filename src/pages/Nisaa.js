73ddbcfb8c2cac3a0d1fb4baeb0b2b25ad3f7a5a
Hamidou.S <86967645+hamidousow@users.noreply.github.com>
2021-11-14 17:36:41 +0100

    nissabox project developement

--- src/pages/Nisaa.js
+++ src/pages/Nisaa.js
@@ -7,6 +7,9 @@ import RichText from '../components/richText/RichText';
 import nisaaBoxDesktop  from '../images/nisaaDesktop.jpg';
 import nisaaFullOne  from '../images/nisaaFullOne.jpg';
 import nisaaFullTwo  from '../images/nisaaFullTwo.jpg';
+import nissaMobile  from '../images/nissaMobile.jpg';
+//import CardProject from '../components/cardProject/CardProject';
+import Footer from '../components/footer/Footer';
 
 
 const Nisaa = () => {
@@ -14,27 +17,40 @@ const Nisaa = () => {
         <>
         <Nav/>
         <Header title={nisaaData.header.title.text}
-                subtitle={nisaaData.header.subtitle.text}
-                
+                subtitle={nisaaData.header.subtitle.text}                
                 alt={nisaaData.header.image.alt}    
                 className={nisaaData.header.image.className}/>
-        <Section sectionTitle={nisaaData.richText.title}> 
+        <Section className={"section"} sectionTitle={nisaaData.richText.title}> 
             <RichText text={nisaaData.richText.text} />
         </Section>
-        <section className="section sectionMockup">
+        <Section className={"section sectionMockup"}>
             <figure className="mockup">
                 <img alt="hero section Nisaabox" src={nisaaBoxDesktop}/>
             </figure>
             <figure className="mockup mockupFull">
                 <img alt="maquette format desktop partie 1/2" src={nisaaFullOne}/>
-                <img alt="maquette format desktop partie 1/2" src={nisaaFullTwo}/>                
+                <img alt="maquette format desktop partie 2/2" src={nisaaFullTwo}/>                
             </figure>
-            <figure className="mockup">
-                <img alt="maquette format desktop partie 1/2" src="#"/>
-                <img alt="maquette format desktop partie 2/2" src="#"/>
-                <img alt="maquette format desktop partie 2/2" src="#"/>
-            </figure>
-        </section>
+            <figure className="mockup mockupMobile">
+                <img alt="mobile mockup" src={nissaMobile}/>
+                <img alt="mobile mockup" src={nissaMobile}/>
+                <img alt="mobile mockup" src={nissaMobile}/>
+            </figure>            
+        </Section>
+        <Section className={"section sectionMoreDetails"}>
+            <h2 className="smallTitle">more details</h2>
+            <div className="link">
+                <a className="" href="">intagram</a>
+            </div>
+        </Section>
+
+        <Section className={"section"}>
+            <div className="content">
+                <h2 className="bigTitle">next project</h2>
+            </div>
+            {/* <CardProject/> */}
+        </Section>
+        <Footer/>
         </>
         
     );
