172bae0bec2c7a9aec14228d91ba1f5ce5634fb3
Hamidou.S <86967645+hamidousow@users.noreply.github.com>
2021-11-15 15:10:48 +0100

    viewmore section created

--- src/pages/Nisaa.js
+++ src/pages/Nisaa.js
@@ -8,9 +8,11 @@ import nisaaBoxDesktop  from '../images/nisaaDesktop.jpg';
 import nisaaFullOne  from '../images/nisaaFullOne.jpg';
 import nisaaFullTwo  from '../images/nisaaFullTwo.jpg';
 import nissaMobile  from '../images/nissaMobile.jpg';
-//import CardProject from '../components/cardProject/CardProject';
+import background from '../images/nisaa-header.jpg';
 import Footer from '../components/footer/Footer';
-
+import ViewMore from '../components/viewMore/ViewMore';
+import Card from '../components/card/Card';
+//import cardData from '../components/cardProject/cardData.json';
 
 const Nisaa = () => {
     return (
@@ -18,15 +20,17 @@ const Nisaa = () => {
         <Nav/>
         <Header title={nisaaData.header.title.text}
                 subtitle={nisaaData.header.subtitle.text}                
-                alt={nisaaData.header.image.alt}    
-                className={nisaaData.header.image.className}/>
-        <Section className={"section"} sectionTitle={nisaaData.richText.title}> 
-            <RichText text={nisaaData.richText.text} />
+                alt={nisaaData.header.image.alt}               
+                background={{backgroundImage: `url(${background})`, backgroundSize: 'cover', backgroundRepeat: 'no-repeat', backgroundPosition: 'center'}}/>
+        <Section className={"section intro"} sectionTitle={nisaaData.richText.title}> 
+            <RichText text={nisaaData.richText.text}/>
         </Section>
-        <Section className={"section sectionMockup"}>
-            <figure className="mockup">
+        <Section className={"section"}>
+            <ViewMore link={"./"} source={nisaaData.header.image.source}>
                 <img alt="hero section Nisaabox" src={nisaaBoxDesktop}/>
-            </figure>
+            </ViewMore>
+        </Section>
+        <Section className={"section sectionMockup"}>            
             <figure className="mockup mockupFull">
                 <img alt="maquette format desktop partie 1/2" src={nisaaFullOne}/>
                 <img alt="maquette format desktop partie 2/2" src={nisaaFullTwo}/>                
@@ -36,20 +40,20 @@ const Nisaa = () => {
                 <img alt="mobile mockup" src={nissaMobile}/>
                 <img alt="mobile mockup" src={nissaMobile}/>
             </figure>            
-        </Section>
-        <Section className={"section sectionMoreDetails"}>
-            <h2 className="smallTitle">more details</h2>
-            <div className="link">
-                <a className="" href="">intagram</a>
-            </div>
-        </Section>
-
-        <Section className={"section"}>
-            <div className="content">
-                <h2 className="bigTitle">next project</h2>
-            </div>
-            {/* <CardProject/> */}
-        </Section>
+        </Section>        
+        <Section className={"section nextProject"}>            
+            <h2 className="bigTitle" style={{marginBottom: "1em"}}>next project</h2>
+            <Card number={"01"}
+                title={"titre prochain"}
+                subtitle = {"sous titre"}
+                cardLink = {"#"}
+                source={"./img/miae-header.jpg"}
+                alt = {"alt"}/> 
+                
+                <span style={{display: 'flex', justifyContent: 'flex-end'}}>
+                    <a className="smallTitle" href="#" style={{width: 'fit-content'}}>all projects</a>
+                </span>            
+        </Section>         
         <Footer/>
         </>
         
