3c94a7ade864686f74eaf67d3cd775d465d9fbce
Hamidou.S <86967645+hamidousow@users.noreply.github.com>
2021-11-15 15:08:07 +0100

    header bg and padding

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -9,9 +9,7 @@ const Header = (props) => {
                     <h1 className="bigTitle"> {props.title} </h1> 
                     <p className="smallTitle">{props.subtitle}</p>          
                 </div>
-                <div className="rightContent">
-                    <img src={props.image} className={props.className} alt={props.alt}/>
-                </div>
+                <div className="rightContent" style={props.background}></div>
             </div>           
             <CallToAction/>
         </header>
