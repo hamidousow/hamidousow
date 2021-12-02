000205fa831d0163fef6c1c99ba02e8315353bbe
Hamidou.S <86967645+hamidousow@users.noreply.github.com>
2021-12-02 16:41:17 +0100

    rich text content modified

--- src/components/richText/RichText.js
+++ src/components/richText/RichText.js
@@ -1,14 +1,16 @@
 import React from 'react';
 
-const RichText = (props) => {
+const RichText = (props) => {    
+
     return (
         <>
-            <div className="leftContent">
-                <p className="text">{props.text}</p>
+            <div className="leftContent">                
+                {/* <p className="text">{props.text}</p> */}
+                {props.children}
             </div>
-            <div className="rightContent">
+            {/* <div className="rightContent">
                 <img className="ImgContent" alt={props.alt}/>
-            </div>            
+            </div>*/}
         </>
     );
 };
