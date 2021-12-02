8db98f2fd9688642f09f46d8fc7992950f18fbf2
Hamidou.S <86967645+hamidousow@users.noreply.github.com>
2021-12-02 16:41:53 +0100

    component mailto added to contact section

--- src/components/contact/Contact.js
+++ src/components/contact/Contact.js
@@ -1,17 +1,25 @@
 import React from 'react';
 import Button from '../button/Button';
 import RichText from '../richText/RichText';
-import { contactData } from '../contact/contactData';
 import WrapTitle from '../wrapTitle/WrapTitle';
 import Section from '../section/Section';
+import ButtonMailto from '../btnMailto/ButtonMailto';
 
 const Contact = () => {
     return (
         <Section className={"section"} id={"contact"}>
             <WrapTitle sectionTitle={"contact"} />
-            <RichText text={contactData.text}/>
+            <RichText>
+                <p className="text">
+                    I'm available now, if you search a ambitious and creative web developer
+                </p>
+            </RichText>
+
             <div className="containerButton">
-                <Button text={'contact me'} mailto={"hamidou.sow@hotmail.fr"}/>
+                {/* <Button text={'contact me'} mailto={"hamidou.sow@hotmail.fr"}/> */}
+                <ButtonMailto className="button" 
+                                text="contact me" 
+                                mailto="mailto:hamidou.sow@hotmail.fr"/>
                 <Button text={'cv online'} href={"https://hamidousow.github.io/hamidou-cv-online/"}/>
             </div>            
         </Section>
