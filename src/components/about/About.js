b114aacc0e4b7d9368453304324d70aad77ddbb4
Hamidou.S <86967645+hamidousow@users.noreply.github.com>
2022-03-18 03:28:21 +0100

    Update About.js

--- src/components/about/About.js
+++ src/components/about/About.js
@@ -10,7 +10,7 @@ const About = () => {
             <WrapTitle sectionTitle={"about"}/>
             <RichText>
                 <p className="text">
-<<<<<<< HEAD
+
                 Développeur à l’écoute, agile, créatif avec une appétence pour le design.
                 </p>
                 <br/>
@@ -21,17 +21,7 @@ const About = () => {
                 <br/>
                 <p className="text">
                 Je suis disponible dès le mois d’avril pour un premier stage d’une durée de 6 semaines, avec comme perspective d’intégrer l’entreprise accueillante en contrat de professionnalisation.
-=======
-                    Développeur à l’écoute, agile, créatif avec une appétence pour le design.
-                </p>
-                <br/>
-                <p className="text">
-                    Dans le cadre de ma formation de concepteur développeur d’applications, je souhaite intégrer une équipe qui me permettra de monter en compétences à travers des projets enrichissants.
-                </p>
-                <br/>
-                <p className="text">
-                    Je suis disponible dès le mois d’avril pour un premier stage d’une durée de 6 semaines, avec comme perspective d’intégrer l’entreprise accueillante en contrat de professionnalisation.
->>>>>>> 89e915576560ad93c6556621447415af1199fc64
+
                 </p>
             </RichText>
         </Section>        
