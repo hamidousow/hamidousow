7474103a0a3be27162f580e96c4f0a4d957563c4
Hamidou.Sow <hamidou.sow@hotmail.fr>
2022-03-18 02:42:48 +0100

    update introduction

--- src/components/about/aboutData.js
+++ src/components/about/aboutData.js
@@ -1,10 +1,7 @@
 export const aboutData = {
-    text: ["My  name's Hamidou SOW, i'm a french web designer and front-end developer.",
-
-        "Web design makes me discover the developement. I always wish give life to my design so I started learn the basics languages to develop and it become a passion through the differents projects.", 
-        
-        "Creative curious and nosy, my objectif is to work for compagnies to learn more technologies and improve my knolewdge in developement and design."],
-
+    text: ["Développeur à l’écoute, agile, créatif avec une appétence pour le design.",
+            "Dans le cadre de ma formation de concepteur développeur d’applications, je souhaite intégrer une équipe qui me permettra de monter en compétences à travers des projets enrichissants.",
+            "Je suis disponible dès le mois d’avril pour un premier stage d’une durée de 6 semaines, avec comme perspective d’intégrer l’entreprise accueillante en contrat de professionnalisation."],
     img: { 
         src: "./img/heroImg.svg"                  
     }
--- src/components/heroSection/HeroSection.js
+++ src/components/heroSection/HeroSection.js
@@ -11,8 +11,8 @@ const HeroSection = () => {
                 <div className="leftContent">
                     <h1 className="bigTitle">
                         <span className="gradient-title"> Hello. </span>
-                        <br/>I am Hamidou</h1> 
-                    <p className="smallTitle">developer & ui / ux designer</p>          
+                        <br/>Je suis Hamidou</h1> 
+                    <p className="smallTitle">Concepteur développeur d'applications</p>          
                 </div>
                 <div className="rightContent">
                     <img src={heroImg} className="heroImg" alt="Hamidou SOW"/>
