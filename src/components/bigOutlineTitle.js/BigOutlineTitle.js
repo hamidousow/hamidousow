95305e2888a7064d70c4a711961fb4eec4c98873
Hamidou.Sow <hamidou.sow@hotmail.fr>
2022-05-04 16:12:34 +0200

    delete react app and integrate static website vanilla js and html/css

--- src/components/bigOutlineTitle.js/BigOutlineTitle.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-const BigOutlineTitle = () => {
-    return (
-        <span className="bigOutlineTitle outlines">{props.text}</span>
-    );
-};
-
-export default BigOutlineTitle;
