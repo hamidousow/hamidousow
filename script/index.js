ff76ab7d161c1f94f3d71cb3f8015bd0ca6a3b8c
Hamidou.Sow <hamidou.sow@hotmail.fr>
2022-08-27 03:01:47 +0200

    big reset

--- script/index.js
+++ script/index.js
@@ -53,63 +53,6 @@ let cardData = fetch("../data/projectData.json")
     })
     .then(data => createNewNode(data))
 
-let outilsData = fetch("../data/outilsData.json")
-    .then(response => {
-        return response.json();
-    })
-    .then(data =>                        
-        createOutil(data)                                              
-    );
-
-let experienceData = fetch("../data/experienceData.json")
-    .then(response => {
-        return response.json();
-        
-    })
-    .then(data =>                        
-        createExperienceArticle(data)                                         
-    );
-
-let formation = fetch("../data/formationData.json")
-    .then(response => {
-        return response.json();        
-    })
-    .then(data =>                        
-        formationNewData = createFormationArticle(data)                                                
-    );
-
-/* FUNCTIONS */
-
-let article = document.querySelector(".exp__article");
-
-let createExperienceArticle = (item) => {
-    let newArticle;
-    for(let i = 0; i < item.length; i++) {         
-        newArticle = article.cloneNode(true);
-        article.setAttribute("id", item[i].id);
-        document.querySelector(".article__year").innerHTML = item[i].year;    
-        document.querySelector(".article__month").innerHTML = item[i].month;      
-        document.querySelector(".article__title").innerHTML = item[i].title;
-        document.querySelector(".article__subtitle").innerHTML = item[i].subtitle;
-        document.querySelector(".article__description").innerHTML = item[i].taskDescription;        
-        article.after(newArticle);
-    }    
-}
-
-let formationArticle = document.querySelector(".formation__article");
-let createFormationArticle = (item) => {
-    let newFormationArticle;
-    for(let i = 0; i < item.length; i++) {         
-        newFormationArticle = formationArticle.cloneNode(true);
-        formationArticle.setAttribute("id", item[i].id);
-        document.querySelector(".formation__article__year").innerHTML = item[i].year;    
-        document.querySelector(".formation__article__month").innerHTML = item[i].month;      
-        document.querySelector(".formation__article__title").innerHTML = item[i].title;
-        document.querySelector(".formation__article__subtitle").innerHTML = item[i].subtitle;
-        formationArticle.after(newFormationArticle);
-    }    
-}
-
 /** 
  * clone card element for each iteration on the json file
  */
@@ -129,16 +72,7 @@ let createNewNode = (item) => {
     card.after(newCard)    
 }
 
-let createOutil = (item) => {
 
-    for(let i = 0; i < item.length; i++) {
-        let outilImg = document.createElement('img');
-        document.getElementById("outils-main-container").append(outilImg);
-        outilImg.setAttribute('class', "logo_tech");
-        outilImg.src = item[i].src;
-        outilImg.alt = item[i].alt;
-    }       
-}
 
 
 
