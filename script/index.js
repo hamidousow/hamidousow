dbfdaaa77b6ca56c1e29f064df4e4bbaa9fbd1bc
Hamidou.Sow <hamidou.sow@hotmail.fr>
2022-11-05 09:04:15 +0100

    refactoring projects cards mapping, updating navigation, rename classlists

--- script/index.js
+++ script/index.js
@@ -1,3 +1,7 @@
+(function () {
+    getProjects()
+})()
+
 /**
  * set navigation opacity when scrolling the page from the top, 
  * by adding the class "active" to the nav classlist
@@ -17,10 +21,11 @@ window.addEventListener('scroll', setNavOpacity);
 /**
  * open/close aside menu on mobile devices 
  */
-
+const navLink = document.querySelectorAll('.navigation__menu__link');
+const menu = document.getElementById("navigation__menu");
 
 let toggleMenu = () => {
-    document.getElementById("menuMobile").classList.toggle("active");
+    menu.classList.toggle("navigation__menu-active");
 }
 
 document.getElementById("btnBurger").addEventListener('click', toggleMenu);
@@ -28,11 +33,11 @@ document.getElementById("btnBurger").addEventListener('click', toggleMenu);
 /**
  * when the mobile menu is open, event click on a link close it
  */
-let nav__link = document.querySelectorAll("#menuMobile .nav__link");
+let nav__link = document.querySelectorAll(".navigation__menu__link");
 
 for(let link of nav__link){
     link.addEventListener('click', () => {     
-        toggleMenu();
+        menu.classList.remove('navigation__menu-active');
     });
 };
 
@@ -45,33 +50,38 @@ document.getElementById("btnSwitchMode").addEventListener("click", () => {
         .classList.toggle("dark-mode");
 }) 
 
-/** fetch local data */
+/** 
+ * fetch data 
+*/
 
-let cardData = fetch("../data/projectData.json")
-    .then(response => {
-        return response.json()
-    })
-    .then(data => createNewNode(data))
+async function getProjects() {
+    const response = await fetch("../data/projectData.json");
+    const results = await response.json()
+    results.map(result => initProjectCards(result.id, result.title, result.subtitle, result.link, result.imageSource, result.imageAlt))
+    // initProjectCards()
+}
 
 /** 
- * clone card element for each iteration on the json file
+ * mapping cards data project
  */
 
-let card = document.querySelector(".card"); 
-let createNewNode = (item) => {
-    let newCard;
-
-    for(let i = 0; i < item.length; i++) {        
-        newCard = card.cloneNode(true);       
-        document.querySelector(".card__title").innerHTML = item[i].content.title.text;
-        document.querySelector(".card__subtitle").innerHTML = item[i].content.subtitle.text;
-        document.querySelector(".card__img").src = item[i].content.background.source;
-        document.querySelector(".card__img").alt = item[i].content.background.alt;
-        document.querySelector(".blockLink").href = item[i].content.link;      
-    }
-    
-    card.after(newCard)    
-}
+// let card = document.querySelector(".card"); 
+function initProjectCards (id, title, subtitle, link, imageSource, imageAlt) { 
+    return document.getElementById('container-cards-projects').innerHTML += (
+        `<div class="card" data-component="card" id=${id}>
+            <a href=${link} class="blockLink" target="_blank" rel="noopener noreferrer">
+                <div class="card__header">
+                    <img class="card__img" src=${imageSource} alt=${imageAlt}/>
+                </div>
+                <div class="card__body">
+                    <div class="card__project__description">
+                        <h3 class="card__title">${title}</h3>
+                        <h4 class="card__subtitle">${subtitle}</h4>
+                    </div>
+                </div>
+            </a>
+        </div>` )     
+}  
 
 
 
