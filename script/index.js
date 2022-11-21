c7c7bb43e88c8058fdafbb1e325f97d495ac6016
Hamidou.Sow <hamidou.sow@hotmail.fr>
2022-11-21 08:20:29 +0100

    full page modification

--- script/index.js
+++ script/index.js
@@ -69,17 +69,16 @@ async function getProjects() {
 function initProjectCards (id, title, subtitle, link, imageSource, imageAlt) { 
     return document.getElementById('container-cards-projects').innerHTML += (
         `<div class="card" data-component="card" id=${id}>
-            <a href=${link} class="blockLink" target="_blank" rel="noopener noreferrer">
                 <div class="card__header">
-                    <img class="card__img" src=${imageSource} alt=${imageAlt}/>
-                </div>
-                <div class="card__body">
-                    <div class="card__project__description">
+                    <a href=${link} target="_blank" rel="noopener noreferrer" class="card__number">${id}</a>
+                    <a href=${link} target="_blank" rel="noopener noreferrer">
                         <h3 class="card__title">${title}</h3>
                         <h4 class="card__subtitle">${subtitle}</h4>
-                    </div>
-                </div>
-            </a>
+                    </a>
+                </div>              
+                <img class="card__img" src=${imageSource} alt=${imageAlt}/>                    
+                
+            
         </div>` )     
 }  
 
