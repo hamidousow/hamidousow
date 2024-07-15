9f3caf4f4d84a795f0e87e3745d540ee3870cef6
Hamidou.Sow <hamidou.sow@hotmail.fr>
2024-07-15 21:42:04 +0200

    add button in project page; update image project height

--- src/views/ProjectView.vue
+++ src/views/ProjectView.vue
@@ -2,6 +2,7 @@
 import projects  from '@/data/projects';
 import type { Project } from '@/types/Project';
 import { useRoute } from 'vue-router'
+import openNewIcon from '@/assets/images/open_in_new.svg'
 
 const route = useRoute()
 
@@ -21,9 +22,9 @@ let project: Project | null = projectArray[0]
 </script>
 
 <template>
-    <div class="container mb-150 mt-60">
+    <div class="mb-150 mt-60">
         <div v-if="project" class="content">
-            <a :href="project.link">
+            <a :href="project.link" class="background-image">
                 <div class="project-image">
                     <img :src="project.imageSource" :alt="project.imageAlt">
                 </div>
@@ -35,8 +36,19 @@ let project: Project | null = projectArray[0]
                     </div>
                     <h1 class="project-details__title">{{ project.title }} </h1>
                     <p class="project-details__description">{{  project.description }}</p>
+                    <a :href="project.link" target="_blank" class="project__link">Voir le projet en ligne</a>
                 </div>
-                <a :href="project.link" target="_blank" class="project__link">Voir le projet en ligne</a>
+                <div class="container-buttons">
+                    <a href="" class="project-link-btn github-link">
+
+                    </a>
+                    <div class="container-button">
+                        <a :href="project.link" class="button project-link-btn live-link" target="_blank" rel="noreferrer">
+                            <img :src="openNewIcon" alt="open blanck page">
+                        </a>
+                    </div>
+                </div>
+                
             </div>
         </div>
         <div v-else>
@@ -51,16 +63,17 @@ let project: Project | null = projectArray[0]
         display: flex; 
         flex-direction: row;
         gap: 50px;  
-        padding: 130px 0;
+        /* padding: 130px 0; */        
     }
 
     .project-image {
         width: 100%;
-        max-width: 700px;
+        /* max-width: 700px; */
     }
 
     .project-image img {
         width: 100%;
+        height: 100%;
         display: block;
         object-fit: cover;
     }
@@ -68,12 +81,38 @@ let project: Project | null = projectArray[0]
     .project-details {
         display: flex;
         flex-direction: column;
-        justify-content: space-between;
+        justify-content: space-around;
         padding: 50px 25px;
     }
 
     .project__link {
         align-self: flex-end;
+    }
+
+    .container-button {
+        background-color: var(--clr-green);
+        width: 47px;
+        height: 47px;
+        border-radius: 5px;
+    }
+    .container-button .button {
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        height: 100%;
+    }
+
+    @media screen and (min-width: 765px) {
+        .content {
+            height: 80dvh;
+            display: grid;
+            grid-template-columns: 1fr 1fr;
+        }
+
+        .project-image {
+            width: 100%;
+            height: 100%;
+        }
     }
 
 
