cce63ae8f245e6bfb3a8625097b33ff335cbf440
Hamidou.S <86967645+hamidousow@users.noreply.github.com>
2025-01-31 20:58:57 +0100

    Update HomeView.vue

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -26,7 +26,7 @@ import { RouterLink } from "vue-router";
                 <span class="line"></span>
                 <div class="card-details-texts">
                   <h3 class="card-subtitle pixelify-font-medium">Moi c'est Hamidou</h3>
-                  <p>Développeur frontend passionné</p>
+                  <p>Développeur Frontend</p>
                 </div>
                 
               </div>
